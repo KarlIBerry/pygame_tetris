04098b137bc8c348dce8a4fad1ef5d55d2b49e98
karlberry88 <karl.berry@sky.uk>
2024-04-25 15:29:02 +0100

    Refactor Game class in game.py to include Tetromino movement and block positioning

--- code/game.py
+++ code/game.py
@@ -1,67 +1,88 @@
-import pygame
 from settings import *
 from random import choice
-
+from pytimer import Timer
 class Game:
-    def __init__(self):
+	def __init__(self):
 
-        # general
-        self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
-        self.display_surface = pygame.display.get_surface()
-        self.rect = self.surface.get_rect(topleft=(PADDING, PADDING))
-        self.sprites = pygame.sprite.Group()
+		# general
+		self.surface = pygame.Surface((GAME_WIDTH, GAME_HEIGHT))
+		self.display_surface = pygame.display.get_surface()
+		self.rect = self.surface.get_rect(topleft = (PADDING, PADDING))
+		self.sprites = pygame.sprite.Group()
 
-        # lines
-        self.line_surface = self.surface.copy()
-        self.line_surface.fill((0, 255, 0))
-        self.line_surface.set_colorkey((0, 255, 0))
-        self.line_surface.set_alpha(120)
+		# lines
+		self.line_surface = self.surface.copy()
+		self.line_surface.fill((0,255,0))
+		self.line_surface.set_colorkey((0,255,0))
+		self.line_surface.set_alpha(120)
 
-        # tetromino
-        self.teromino = Teromino(choice(list(TETROMINOS.keys())), self.sprites)
+		# tetromino
+		self.tetromino = Tetromino(choice(list(TETROMINOS.keys())), self.sprites)
 
-    def draw_grid(self):
+		# timer
+		self.timers = {
+			'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down)
+		}
+		self.timers['vertical move'].activate()
 
-        for col in range(1, COLUMNS):
-            x = col * CELL_SIZE
-            pygame.draw.line(self.line_surface, LINE_COLOR, (x, 0), (x, self.surface.get_height()), 1)
+	def timer_update(self):
+		for timer in self.timers.values():
+			timer.update()
 
-        for row in range(1, ROWS):
-            y = row * CELL_SIZE
-            pygame.draw.line(self.line_surface, LINE_COLOR, (0, y), (self.surface.get_width(), y))
+	def move_down(self):
+		self.tetromino.move_down()
 
-        self.surface.blit(self.line_surface, (0, 0))
+	def draw_grid(self):
 
-    def run(self):
+		for col in range(1, COLUMNS):
+			x = col * CELL_SIZE
+			pygame.draw.line(self.line_surface, LINE_COLOR, (x,0), (x,self.surface.get_height()), 1)
 
-        # drawing
-        self.surface.fill(GRAY)
-        self.sprites.draw(self.surface)
+		for row in range(1, ROWS):
+			y = row * CELL_SIZE
+			pygame.draw.line(self.line_surface, LINE_COLOR, (0,y), (self.surface.get_width(),y))
 
-        self.draw_grid()
-        self.display_surface.blit(self.surface, (PADDING, PADDING))
-        pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+		self.surface.blit(self.line_surface, (0,0))
 
-class Teromino:
-    def __init__(self, shape, group):
+	def run(self):
 
-        #setup
-        self.block_positions = TETROMINOS[shape]['shape']
-        self.color = TETROMINOS[shape]['color']
+		# update
+		self.timer_update()
+		self.sprites.update()
 
-        #create blocks
-        self.blocks = [Block (group, pos, self.color)for pos in self.block_positions]
+		# drawing
+		self.surface.fill(GRAY)
+		self.sprites.draw(self.surface)
 
+		self.draw_grid()
+		self.display_surface.blit(self.surface, (PADDING,PADDING))
+		pygame.draw.rect(self.display_surface, LINE_COLOR, self.rect, 2, 2)
+
+class Tetromino:
+	def __init__(self, shape, group):
+
+		# setup
+		self.block_positions = TETROMINOS[shape]['shape']
+		self.color = TETROMINOS[shape]['color']
+
+		# create blocks
+		self.blocks = [Block(group, pos, self.color) for pos in self.block_positions]
+
+	def move_down(self):
+		for block in self.blocks:
+			block.pos.y += 1
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, group, pos, color):
-        # general
-        super().__init__(group)
-        self.image = pygame.Surface((CELL_SIZE, CELL_SIZE))
-        self.image.fill((color))
+	def __init__(self, group, pos, color):
 
-        # position
-        self.pos = pygame.Vector2(pos) + BLOCK_OFFSET
-        x = self.pos.x * CELL_SIZE
-        y = self.pos.y * CELL_SIZE
-        self.rect = self.image.get_rect(topleft=(x,y))
+		# general
+		super().__init__(group)
+		self.image = pygame.Surface((CELL_SIZE,CELL_SIZE))
+		self.image.fill(color)
+
+		# position
+		self.pos = pygame.Vector2(pos) + BLOCK_OFFSET
+		self.rect = self.image.get_rect(topleft = self.pos * CELL_SIZE)
+
+	def update(self):
+		self.rect.topleft = self.pos * CELL_SIZE
